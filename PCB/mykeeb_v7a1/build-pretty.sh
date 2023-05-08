00b52b38a1e76782573dc21b53d9402e6a19feb2
Maximilian Huber <gh@maxhbr.de>
2023-05-08 20:18:12 +0200

    update
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- PCB/mykeeb_v7a1/build-pretty.sh
+++ PCB/mykeeb_v7a1/build-pretty.sh
@@ -5,14 +5,20 @@ set -euo pipefail
 cd "$(dirname "$0")/pretty"
 
 while read url; do
+    if ! curl --output /dev/null --silent --head --fail "$url"; then
+        echo "failed to reach: $url"
+        continue
+    fi
     fn="$(basename "$url")"
     if [[ -f "$fn" ]]; then
         continue
     fi
-    licenseURL="$(echo "$url" | sed 's/main.*/main/' | sed 's/master.*/master/')/LICENSE.md"
-
     curl "$url" > "$fn"
-    curl "$licenseURL" > "$fn.LICENSE"
+
+    licenseURL="$(echo "$url" | sed 's/main.*/main/' | sed 's/master.*/master/')/LICENSE.md"
+    if curl --output /dev/null --silent --head --fail "$licenseURL"; then
+        curl "$licenseURL" > "$fn.LICENSE.md"
+    fi
 done <<EOM
 https://raw.githubusercontent.com/GEIGEIGEIST/TOTEM/main/PCB/totem_0-3/TOTEMlib.pretty/xiao-ble-smd-cutout.kicad_mod
 EOM
